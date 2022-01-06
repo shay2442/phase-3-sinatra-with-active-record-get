ca75af889d591b39de29818e34d41feeb65dc429
shaywacker <shay2442@yahoo.com>
2022-01-06 14:22:06 -0600

    commit tree 1d5c162844b670dfb0237a7298bbe4a00a83254b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,47 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # get '/games' do
+  #   # get all the games from the database
+  #   games = Game.all
+  #   # return a JSON response with an array of all the game data
+  #   games.to_json
+  # end
+
+  # get '/games' do
+  #   games = Game.all.order(:title)
+  #   games.do_json
+  # end
+
+  # get '/games' do
+  #   games = Game.all.order(:title).limit(10)
+  #   games.to_json
+  # end
+# end
+
